c3b8125132137d7bf93a45520382c465720ff952
Charlize <charliezhou@outlook.com>
2024-06-01 13:45:06 -0700

    adjust structure

--- client/App.jsx
+++ client/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
-import Slider from './Slider';
+import Slider from './components/Slider';
 import '../public/style.css';
 
 function App() {
