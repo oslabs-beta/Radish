40e1d8c5ec615fe3df51298e5bbc163c4e815fda
SameerSyed99 <61425459+SameerSyed99@users.noreply.github.com>
2024-06-02 16:21:50 -0400

    The dev server
    my attempt

---
+++ dist/bundle3524a5686bf86e375c2f.js
@@ -0,0 +1,14 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+// import logo from '/assets/redish.png'
+
+// const logoImg = document.getElementById('logoImg')
+// logoImg.src = logo
+
+console.log("index.js");
+/******/ })()
+;
+//# sourceMappingURL=bundle3524a5686bf86e375c2f.js.map
---
+++ src/index.js
@@ -0,0 +1,6 @@
+// import logo from '/assets/redish.png'
+
+// const logoImg = document.getElementById('logoImg')
+// logoImg.src = logo
+
+console.log("index.js")
