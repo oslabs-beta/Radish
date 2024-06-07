9d6b6f652a1d4d16df8928703beb21c0ce57fa56
Charlize <charliezhou@outlook.com>
2024-06-06 19:49:55 -0700

    add visualization for master and replica

--- client/src/Visualization.jsx
+++ client/src/Visualization.jsx
@@ -2,9 +2,9 @@ import React, { useRef, useEffect } from "react";
 import * as d3 from "d3";
 import { useSelector } from "react-redux";
 import master_logo from "../asset/redis_logo.svg";
-import replica_logo from "../asset/redis_logo.svg";
-//import redis_logo from "../asset/redis_logo.svg";
-import sentinel_logo from "../asset/sentinel.svg";
+import replica_logo from "../asset/replica_logo.svg";
+
+
 
 const Visualization = () => {
   const svgRef = useRef();
@@ -15,58 +15,89 @@ const Visualization = () => {
   useEffect(() => {
     const width = 800;
     const height = 600;
+    const margin = 50;
+    const shardSize = 30;
+    const replicaSize = 20;
+    const sentinelSize = 25;
     const svg = d3
       .select(svgRef.current)
       .attr("width", width)
       .attr("height", height);
 
-    const data = [];
+    svg.selectAll("*").remove();
 
-    for (let i = 0; i < sentinel; i++) {
-      data.push({
-        type: "sentinel",
-        x: Math.random() * width,
-        y: Math.random() * height,
-      });
-    }
+    const shardData = [];
+    const replicaData = [];
+    const sentinelData = [];
+
+    const totalWidthNeeded = (shard * shardSize) + (shard * replica * replicaSize);
+
+    const horizontalGap = (width - totalWidthNeeded) / (shard + 1);
+
+    // for (let i = 0; i < sentinel; i++) {
+    //   sentinelData.push({
+    //     id: `{sentinel-${i}}`,
+    //     type: "sentinel",
+    //     x: Math.random() * (width - 2 * margin) + margin,
+    //     y: Math.random() * (height - 2 * margin) + margin,
+    //   });
+    // }
 
     for (let i = 0; i < shard; i++) {
-      data.push({
+      shardData.push({
+        id: `{shard-${i}}`,
         type: "shard",
-        x: Math.random() * width,
-        y: Math.random() * height,
+        x: (i + 1) * horizontalGap + i * shardSize,
+        y: height / 2 - shardSize / 2,
       });
     }
-    for (let i = 0; i < replica; i++) {
-      data.push({
-        type: "replica",
-        x: Math.random() * width,
-        y: Math.random() * height,
-      });
-    }
+    shardData.forEach((shard, i) => {
+      const offset = shardSize + 10;
+      for (let j = 0; j < replica; j++) {
+        replicaData.push({
+            id: `replica-${i}-${j}`,
+            type: "replica",
+            x: shard.x + j * replicaSize,
+            y: shard.y + offset,
+          });
+      }
+    });
+
+    const allData = [...shardData, ...replicaData];
+
+    
 
     const images = svg
       .selectAll("image")
-      .data(data, (d) => `${d.type}-${d.x}-${d.y}`);
-
-    images
+      .data(allData)
       .enter()
       .append("image")
       .attr("x", (d) => d.x)
       .attr("y", (d) => d.y)
-      .attr("width", 20)
-      .attr("height", 20)
+      .attr("width", (d) =>
+        d.type === "shard"
+          ? shardSize
+          : d.type === "replica"
+          ? replicaSize
+          : sentinelSize
+      )
+      .attr("height", (d) =>
+        d.type === "shard"
+          ? shardSize
+          : d.type === "replica"
+          ? replicaSize
+          : sentinelSize
+      )
       .attr("href", (d) => {
-        if (d.type === "sentinel") return sentinel_logo;
         if (d.type === "shard") return master_logo;
         if (d.type === "replica") return replica_logo;
+        // return sentinel_logo;
       });
-    images.exit().remove();
 
     return () => {
-      <svg ref={svgRef}></svg>;
+      svg.selectAll("*").remove();
     };
-  }, [sentinel, shard, replica]);
+  }, [shard, replica]);
   return <svg ref={svgRef}></svg>;
 };
 
