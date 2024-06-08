7d5b5abfcc9408986f8468e1230144909063a55d
basswoman <54603761+basswoman@users.noreply.github.com>
2024-06-08 15:59:16 -0400

    sidebar

--- client/App.jsx
+++ client/App.jsx
@@ -1,11 +1,12 @@
-import React from "react";
-import Slider from "./components/Slider";
-import Visual from "./components/Visual";
-import "../public/style.css";
+import React from 'react';
+import Slider from './components/Slider';
+import Visual from './components/Visual';
+import Sidebar from './components/Sidebar';
 
 function App() {
   return (
     <div>
+      <Sidebar />
       <h1>radish is running</h1>
       <Slider />
       <Visual />
