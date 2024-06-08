1e96766815123d883b7f94b94c04939a49792e89
Jay Hoogheem <jayh1186@gmail.com>
2024-06-08 16:57:43 -0500

    added docker volumes to each redis container to  persist data

--- server/server.js
+++ server/server.js
@@ -49,76 +49,89 @@ if (process.env.USE_REDIS === 'true') {
 
 // POST route to handle form submission and write redis.conf file
 app.post('/createFiles', (req, res) => {
-    console.log(req.body);
-    // Destructure form data on req.body
-    const { numShards, numReplicas, protectedMode, portNumber, replicaOf, masterAuth, requirePass } = req.body;
+  console.log(req.body);
+  // Destructure form data on req.body
+  let { numShards, numReplicas, protectedMode, portNumber, masterAuth, requirePass } = req.body;
 
-    // Construct the output directory path
-    const outputDir = path.join(__dirname, 'output');
+  // Construct the output directory path
+  const outputDir = path.join(__dirname, 'output');
 
-    // Determine number of redis.conf files to write and initialize file counter
-    let redisFilesToWrite = numShards * numReplicas;
-    let redisFilesWritten = 0;
+  // Determine number of redis.conf files to write and initialize file counter
+  let redisFilesToWrite = numShards * numReplicas;
+  let redisFilesWritten = 0;
 
-    let port = portNumber;
+  let port = Number(portNumber);
 
-    let dockerCompose = {
-        version: '3.8',
-        services: {}
-    };
-    dockerCompose.services['app'] = {
-        image: 'client-app-js:latest',
-        container_name: 'client-app-js',
-        ports: ['80:80'],
-        environment: ['REDIS_HOST:Redis-0-0',`REDIS_PORT:${portNumber}`, `REDIS_PASSWORD:${masterAuth}` ],
-        command: ['sh', '-c', 'sleep 10 && npm start']
+  let dockerCompose = {
+      version: '3.8',
+      services: {},
+      volumes: {}
+  };
+  dockerCompose.services['app'] = {
+      image: 'client-app-js:latest',
+      container_name: 'client-app-js',
+      ports: ['80:80'],
+      environment: ['REDIS_HOST=Redis-0-0',`REDIS_PORT=${portNumber}`, `REDIS_PASSWORD=${masterAuth}`, 'USE_REDIS=true' ],
+      command: ['sh', '-c', 'sleep 10 && npm start']
+  }
+  
+  for (let i = 0; i < numShards; i++) {
+    for (let j = 0; j < numReplicas; j++) {
+      let serviceName = `redis-${i}-${j}`;
+      let masterServiceName = `redis-${i}-0`;
+      
+      // Construct the content for redis.conf
+      let redisConfigContent = `protected-mode ${protectedMode}\nport ${port}\n`;
+
+      // The first config file is for a master node and does not have the replicaof property
+      if (j !== 0) redisConfigContent += `replicaof ${masterServiceName} ${port - j}\n`;
+
+      redisConfigContent += `# Authentication\nmasterauth ${masterAuth}\nrequirepass ${requirePass}\n`;
+
+      // Write each redis configuration file to the output folder
+      let outputFile = path.join(outputDir, `redis-${i}-${j}.conf`);
+      fs.writeFile(outputFile, redisConfigContent, (err) => {
+          if (err) {
+              console.error('Error writing redis.conf file(s):', err);
+              return res.status(500).json({ error: 'Internal Server Error' });
+          }
+          redisFilesWritten++;
+          // Check if the required number of files have been written before responding to the client
+          if (redisFilesWritten === redisFilesToWrite) {
+              // Write the require docker-compose.yml file
+              const dockerComposeFile = path.join(outputDir, 'docker-compose.yml');
+              fs.writeFile(dockerComposeFile, yaml.dump(dockerCompose), (err) => {
+                  if (err) {
+                      console.error('Error writing docker-compose.yml file:', err);
+                  }
+                  res.status(200).json({ message: `All configuration files written successfully to path: ${outputDir}` });
+              });
+          }
+        });
+
+        // Define volume(s) to ensure data persistence after docker containers are stopped and removed.
+        let volumeName = `redis-${i}-${j}-data`;
+
+        // Add each redis instance to the docker-compose configuration
+        dockerCompose.services[`redis-${i}-${j}`] = {
+            image: 'redis:latest',
+            container_name: serviceName,
+            volumes: [
+                `./redis-${i}-${j}.conf:/usr/local/etc/redis/redis.conf`,
+                `${volumeName}:/data`
+            ],
+            command: `redis-server /usr/local/etc/redis/redis.conf`,
+            ports: [`${port}:${port - j}`]
+        };
+
+        // Add volume to the volumes section of the docker-compose file
+        dockerCompose.volumes[volumeName] = {};
+
+        // Increment port number as each Redis instance will run on a different port
+        port++;
+      }
     }
-
-    for (let i = 0; i < numShards; i++) {
-        for (let j = 0; j < numReplicas; j++) {
-            // Construct the content for redis.conf
-            let redisConfigContent = `protected-mode ${protectedMode}\nport ${port}\n`;
-
-            // The first config file is for a master node and does not have the slaveof property
-            if (j !== 1) redisConfigContent += `replicaof ${replicaOf} ${port}\n`;
-
-            redisConfigContent += `# Authentication\nmasterauth ${masterAuth}\nrequirepass ${requirePass}\n`;
-
-            // Write each redis configuration file to the output folder
-            let outputFile = path.join(outputDir, `redis-${i}-${j}.conf`);
-            fs.writeFile(outputFile, redisConfigContent, (err) => {
-                if (err) {
-                    console.error('Error writing redis.conf file(s):', err);
-                    return res.status(500).json({ error: 'Internal Server Error' });
-                }
-                redisFilesWritten++;
-                // Check if the required number of files have been written before responding to the client
-                if (redisFilesWritten === redisFilesToWrite) {
-                    // Write the require docker-compose.yml file
-                    const dockerComposeFile = path.join(outputDir, 'docker-compose.yml');
-                    fs.writeFile(dockerComposeFile, yaml.dump(dockerCompose), (err) => {
-                        if (err) {
-                            console.error('Error writing docker-compose.yml file:', err);
-                        }
-                        res.status(200).json({ message: `All configuration files written successfully to path: ${outputDir}` });
-                    });
-                }
-             });
-
-            // Add each redis instance to the docker-compose configuration
-            dockerCompose.services[`redis-${i}-${j}`] = {
-                image: 'redis:latest',
-                container_name: `redis-${i}-${j}`,
-                volumes: [`./output/redis-${i}-${j}.conf:/usr/local/etc/redis/redis.conf`],
-                command: `redis-server /usr/local/etc/redis/redis.conf`,
-                ports: [`${port}:${portNumber}`]
-            };
-
-            // Increment port number as each Redis shard will run on a different port
-            port++;
-        }
-    }
-});
+  });
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
