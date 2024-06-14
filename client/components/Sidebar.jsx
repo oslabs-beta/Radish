4d58602cf2cdcfb62b8a73fcfb4adda7fe150505
Charlize <charliezhou@outlook.com>
2024-06-13 20:13:36 -0700

    merge sidebar

--- client/components/Sidebar.jsx
+++ client/components/Sidebar.jsx
@@ -7,27 +7,43 @@ import { IoIosMenu } from "react-icons/io";
 
 function Sidebar() {
   return (
-    <nav className="bg-gray-800 text-white w-64 min-h-screen">
+    <nav className="bg-white text-black w-64 min-h-screen shadow-lg">
       <div className="px-4 py-6">
         <ul className="space-y-4">
           <li>
-            <a href="#" className="text-gray-300 hover:text-white">
-              Configuration
+            <a
+              href="#"
+              className="flex items-center space-x-2 text-gray-700 hover:text-black"
+            >
+              <GiDiamondTrophy className="text-xl" />
+              <span>Configuration</span>
             </a>
           </li>
           <li>
-            <a href="#" className="text-gray-300 hover:text-white">
-              Pricing
+            <a
+              href="#"
+              className="flex items-center space-x-2 text-gray-700 hover:text-black"
+            >
+              <MdOutlineMonitorHeart className="text-xl" />
+              <span>Pricing</span>
             </a>
           </li>
           <li>
-            <a href="#" className="text-gray-300 hover:text-white">
-              Performance
+            <a
+              href="#"
+              className="flex items-center space-x-2 text-gray-700 hover:text-black"
+            >
+              <GrDocumentConfig className="text-xl" />
+              <span>Performance</span>
             </a>
           </li>
           <li>
-            <a href="#" className="text-gray-300 hover:text-white">
-              Docker
+            <a
+              href="#"
+              className="flex items-center space-x-2 text-gray-700 hover:text-black"
+            >
+              <FaDocker className="text-xl" />
+              <span>Docker</span>
             </a>
           </li>
         </ul>
